8aeec6f091b34b46605b9d42faa78fa84ba9c9bc
frakibuddin <frakibuddin360@gmail.com>
2023-09-26 12:27:18 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // header mobile menu function
 let toggle_btn = document.querySelector(".toggle-btn");
-let menu = document.querySelector("menu");
+let menu = document.querySelector(".menu");
 
 toggle_btn.onclick = () => {
   toggle_btn.classList.toggle("active");
